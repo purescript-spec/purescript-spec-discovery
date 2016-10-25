8acd93a116cb174c9133fb7af9b4155f132124fc
Oskar Wickström <oskar.wickstrom@gmail.com>
2016-10-25 21:43:38 +0200

    Add FS effect to discovery

--- src/Test/Spec/Discovery.purs
+++ src/Test/Spec/Discovery.purs
@@ -3,11 +3,12 @@ module Test.Spec.Discovery (discover) where
 import Prelude
 import Control.Monad.Eff (Eff)
 import Data.Traversable (sequence_)
+import Node.FS (FS)
 import Test.Spec (Spec)
 
 foreign import getSpecs :: forall r. String
-                        -> Eff r (Array (Spec r Unit))
+                        -> Eff (fs :: FS | r) (Array (Spec (fs :: FS | r) Unit))
 
 discover :: forall r. String
-         -> Eff r (Spec r Unit)
+         -> Eff (fs :: FS | r) (Spec (fs :: FS | r) Unit)
 discover pattern =  getSpecs pattern >>= (pure <<< sequence_)
--- test/Main.purs
+++ test/Main.purs
@@ -3,10 +3,11 @@ module Test.Main where
 import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE)
+import Node.FS (FS)
 import Node.Process (PROCESS)
 import Test.Spec.Discovery (discover)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (run)
 
-main :: Eff (process :: PROCESS, console :: CONSOLE) Unit
+main :: Eff (fs :: FS, process :: PROCESS, console :: CONSOLE) Unit
 main = discover "Test\\.Spec\\.Discovery\\..*Spec" >>= run [consoleReporter]
