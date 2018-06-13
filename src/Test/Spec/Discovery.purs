dc0a974b1dc2e9a95e1297ee05bc6b1512a1ef44
Paul Young <paulyoungonline@gmail.com>
2018-06-12 23:23:00 -0700

    Updates for 0.12

--- src/Test/Spec/Discovery.purs
+++ src/Test/Spec/Discovery.purs
@@ -1,14 +1,14 @@
 module Test.Spec.Discovery (discover) where
 
 import Prelude
-import Control.Monad.Eff (Eff)
+
 import Data.Traversable (sequence_)
-import Node.FS (FS)
+import Effect (Effect)
 import Test.Spec (Spec)
 
-foreign import getSpecs :: forall r. String
-                        -> Eff (fs :: FS | r) (Array (Spec (fs :: FS | r) Unit))
+foreign import getSpecs :: String
+                        -> Effect (Array (Spec Unit))
 
-discover :: forall r. String
-         -> Eff (fs :: FS | r) (Spec (fs :: FS | r) Unit)
-discover pattern =  getSpecs pattern >>= (pure <<< sequence_)
+discover :: String
+         -> Effect (Spec Unit)
+discover pattern = getSpecs pattern >>= (pure <<< sequence_)
