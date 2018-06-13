040d83f809b3ccfb742be7926af2ea9dd5cc2f8a
Paul Young <paulyoungonline@gmail.com>
2018-06-12 23:23:00 -0700

    Updates for 0.12

--- test/Test/Spec/Discovery2Spec.purs
+++ test/Test/Spec/Discovery2Spec.purs
@@ -3,7 +3,7 @@ module Test.Spec.Discovery2Spec where
 import Prelude
 import Test.Spec (Spec, describe, it)
 
-spec :: forall r. Spec r Unit
+spec :: Spec Unit
 spec =
   describe "discovery" do
     it "discovers the specs again in this project" $ pure unit
--- test/Test/Spec/DiscoverySpec.purs
+++ test/Test/Spec/DiscoverySpec.purs
@@ -3,7 +3,7 @@ module Test.Spec.DiscoverySpec where
 import Prelude
 import Test.Spec (Spec, describe, it)
 
-spec :: forall r. Spec r Unit
+spec :: Spec Unit
 spec =
   describe "discovery" do
     it "discovers the specs in this project" $ pure unit
