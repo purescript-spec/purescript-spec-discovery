2a35c773bb0d7275b42a9a7ed26cf3a4ab9b21b7
Carsten König <Carsten.Koenig@hotmail.de>
2022-05-30 13:02:42 +0200

    [fix]Discovery on Windows using file:// URL (#31)

--- src/Test/Spec/Discovery.js
+++ src/Test/Spec/Discovery.js
@@ -1,30 +1,42 @@
-import path from "path"
-import fs from 'fs';
-import { fileURLToPath } from "url"
+import path from 'path'
+import fs from 'fs'
+import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+const __onWindows = process.platform === 'win32' || process.platform === 'win64'
 
 if (import.meta.url === `file://${process.argv[1]}`) {
-    throw new Error('Sorry, purescript-spec-discovery only supports NodeJS environments!');
+  throw new Error(
+    'Sorry, purescript-spec-discovery only supports NodeJS environments!'
+  )
 }
 
-function getMatchingModules(pattern) {
-  var directories = fs.readdirSync(path.join(__dirname, '..'));
-  const modulePromises = directories.filter(function (directory) {
-    return (new RegExp(pattern).test(directory));
-  }).map(function (name) {
-    var modulePromise = import(path.join(__dirname, '..', name, 'index.js'));
-    return modulePromise.then( module => {
-      return (module && typeof module.spec !== 'undefined') ? module.spec : null;
+function getMatchingModules (pattern) {
+  var directories = fs.readdirSync(path.join(__dirname, '..'))
+  const modulePromises = directories
+    .filter(function (directory) {
+      return new RegExp(pattern).test(directory)
     })
-  })
+    .map(function (name) {
+      const fullPath = __onWindows
+        ? path.join('file://', __dirname, '..', name, 'index.js')
+        : path.join(__dirname, '..', name, 'index.js')
+      var modulePromise = import(fullPath)
+      return modulePromise.then(module => {
+        return module && typeof module.spec !== 'undefined' ? module.spec : null
+      })
+    })
   const modules = Promise.all(modulePromises)
-  return modules.then(ms => ms.filter(function (x) { return x; }));
+  return modules.then(ms =>
+    ms.filter(function (x) {
+      return x
+    })
+  )
 }
 
-export function getSpecs(pattern) {
+export function getSpecs (pattern) {
   return function () {
-    return getMatchingModules(pattern);
-  };
+    return getMatchingModules(pattern)
+  }
 }
